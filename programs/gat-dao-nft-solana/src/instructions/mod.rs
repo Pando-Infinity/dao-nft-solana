71e470767275fb1e4418115a43d2a875e1378669
tuan.ld <tuan.ledinh@ddsolution.vn>
2022-12-15 15:59:18 +0700

    add mod.rs

---
+++ programs/gat-dao-nft-solana/src/instructions/mod.rs
@@ -0,0 +1,8 @@
+pub mod writers;
+pub use writers::*;
+
+pub mod governance;
+pub use governance::*;
+
+pub mod readers;
+pub use readers::*;
---
+++ programs/gat-dao-nft-solana/src/schemas/mod.rs
@@ -0,0 +1,8 @@
+pub mod governance_info;
+pub use governance_info::*;
+
+pub mod dao_info;
+pub use dao_info::*;
+
+pub mod access_control;
+pub use access_control::*;
